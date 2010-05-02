45bb535c0ba239e893f1143634ca0721b205bd8b
memememomo <memememomo@gmail.com>
2010-05-02 10:12:12 +0900

    modified eg/sample_memory.pl

--- eg/sample_memory.pl
+++ eg/sample_memory.pl
@@ -7,13 +7,13 @@ use Classifier::Filter::Memory;
 my $dat = './filter.dat';
 
 my $cl = Classifier->new;
-my $filter = Classifier::Filter::Memory->new();
-$cl->set_filter($filter);
 
 # 学習フェーズ
 if (-e $dat) {
     $cl->load_filter($dat);
 } else {
+    my $filter = Classifier::Filter::Memory->new();
+    $cl->set_filter($filter);
     $cl->train('perlやpythonはスクリプト言語です', 'it');
     $cl->train('perlでベイジアンフィルタを作りました', 'it');
     $cl->train('pythonはニシキヘビ科のヘビの総称', 'science');
